1b0e5203f823ad42459d167ca01a4a5b80a3c82a
chenglong.zq <chenglong.zq@gmail.com>
2016-11-04 12:42:38 +0100

    Customizing forms and Styles

--- animals/admin.py
+++ animals/admin.py
@@ -3,8 +3,24 @@ from django.contrib import admin
 from .models import Animal
 from .models import Person
 
+
 # Register the following fields to admin backend
 
-admin.site.register(Person)
-admin.site.register(Animal)
 
+class AnimalAdmin(admin.ModelAdmin):
+    fields = ['animal_name']
+
+
+class PersonAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('SET 1 NAME', {'fields': ['person_name']})
+        ('SET 2 NAME', {'fields': ['person_email']})
+    ]
+    # fields = ['person_name']
+
+
+# admin.site.register(Person)
+# admin.site.register(Animal)
+
+admin.site.register(Animal, AnimalAdmin)
+admin.site.register(Person, PersonAdmin)
